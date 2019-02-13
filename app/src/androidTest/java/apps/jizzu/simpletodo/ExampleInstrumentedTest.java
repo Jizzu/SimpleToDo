dc3086723da379931ce1da3d5470f8afb07f2927
Jizzu <ilya.ponomarenko.dev@gmail.com>
2019-02-13 15:44:25 +0700

    Cleaned up build.gradle dependencies

--- app/src/androidTest/java/apps/jizzu/simpletodo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/apps/jizzu/simpletodo/ExampleInstrumentedTest.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 
 /**
- * Instrumentation getAllTasksLiveData, which will execute on an Android device.
+ * Instrumentation test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
@@ -18,7 +18,7 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
-        // Context of the app under getAllTasksLiveData.
+        // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("apps.jizzu.simpletodo", appContext.getPackageName());
