8acf269b06d316954c07923d0e440c7bfcb0e2ca
Jizzu <ilya.ponomarenko.dev@gmail.com>
2019-04-06 17:12:14 +0700

    Deleted some old stuff

--- app/src/main/java/apps/jizzu/simpletodo/utils/Interpolator.kt
+++
@@ -1,9 +0,0 @@
-package apps.jizzu.simpletodo.utils
-
-class Interpolator(private val mAmplitude: Double, private val mFrequency: Double) : android.view.animation.Interpolator {
-
-    override fun getInterpolation(time: Float): Float {
-        return (-1.0 * Math.pow(Math.E, -time / mAmplitude) *
-                Math.cos(mFrequency * time) + 1).toFloat()
-    }
-}
