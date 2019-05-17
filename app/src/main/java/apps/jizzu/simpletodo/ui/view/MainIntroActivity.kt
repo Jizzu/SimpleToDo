22b983754136de8896312b3da86139d1421736c6
Jizzu <ilya.ponomarenko.dev@gmail.com>
2019-05-17 17:44:55 +0700

    MainIntroActivity refactoring

--- app/src/main/java/apps/jizzu/simpletodo/ui/view/MainIntroActivity.kt
+++ app/src/main/java/apps/jizzu/simpletodo/ui/view/MainIntroActivity.kt
@@ -9,37 +9,21 @@ class MainIntroActivity : IntroActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        addSlide(SimpleSlide.Builder()
-                .title(getString(R.string.intro_1_title))
-                .description(getString(R.string.intro_1_description))
-                .image(R.drawable.illustration_intro_1)
-                .background(R.color.white)
-                .backgroundDark(R.color.white)
-                .layout(R.layout.activity_main_intro)
-                .build())
+        addIntroScreen(getString(R.string.intro_1_title), getString(R.string.intro_1_description),
+                R.drawable.illustration_intro_1)
+        addIntroScreen(getString(R.string.intro_2_title), getString(R.string.intro_2_description),
+                R.drawable.illustration_intro_2)
+        addIntroScreen(getString(R.string.intro_3_title), getString(R.string.intro_3_description),
+                R.drawable.illustration_intro_3)
+        addIntroScreen(getString(R.string.intro_4_title), getString(R.string.intro_4_description),
+                R.drawable.illustration_intro_4)
+    }
 
+    private fun addIntroScreen(title: String, description: String, drawable: Int) {
         addSlide(SimpleSlide.Builder()
-                .title(getString(R.string.intro_2_title))
-                .description(getString(R.string.intro_2_description))
-                .image(R.drawable.illustration_intro_2)
-                .background(R.color.white)
-                .backgroundDark(R.color.white)
-                .layout(R.layout.activity_main_intro)
-                .build())
-
-        addSlide(SimpleSlide.Builder()
-                .title(getString(R.string.intro_3_title))
-                .description(getString(R.string.intro_3_description))
-                .image(R.drawable.illustration_intro_3)
-                .background(R.color.white)
-                .backgroundDark(R.color.white)
-                .layout(R.layout.activity_main_intro)
-                .build())
-
-        addSlide(SimpleSlide.Builder()
-                .title(getString(R.string.intro_4_title))
-                .description(getString(R.string.intro_4_description))
-                .image(R.drawable.illustration_intro_4)
+                .title(title)
+                .description(description)
+                .image(drawable)
                 .background(R.color.white)
                 .backgroundDark(R.color.white)
                 .layout(R.layout.activity_main_intro)
